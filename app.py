5dfef5b5678677cf8af2a00464387ae3e44ef957
Ejtys <jwziebapn@gmail.com>
2023-04-08 21:57:44 +0200

    Playing aroung with chatgpt

---
+++ app.py
@@ -0,0 +1,132 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
+
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+CORS(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash('Username already exists. Please choose a different one.')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Task {self.id} - {self.text}>'
+
+def create_database():
+    with app.app_context():
+        db.create_all()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/tasks', methods=['GET'])
+@login_required
+def get_tasks():
+    tasks = Task.query.filter_by(user_id=current_user.id).all()
+    return jsonify([{'id': task.id, 'text': task.text, 'done': task.done} for task in tasks])
+
+
+@app.route('/tasks', methods=['POST'])
+@login_required
+def add_task():
+    task_data = request.get_json()
+    new_task = Task(text=task_data['text'], done=task_data['done'], user_id=current_user.id)
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'id': new_task.id, 'text': new_task.text, 'done': new_task.done})
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+@login_required
+def update_task(task_id):
+    task_data = request.get_json()
+    task = Task.query.filter_by(id=task_id, user_id=current_user.id).first_or_404()
+    task.done = task_data['done']
+    db.session.commit()
+    return jsonify({'id': task.id, 'text': task.text, 'done': task.done})
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+@login_required
+def delete_task(task_id):
+    task = Task.query.filter_by(id=task_id, user_id=current_user.id).first_or_404()
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'result': 'success'})
+
+
+
+if __name__ == '__main__':
+    create_database()
+    app.run(debug=True)
---
+++ static/app.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('taskInput');
+    const addTaskButton = document.getElementById('addTask');
+    const taskList = document.getElementById('taskList');
+  
+    // Load tasks from the server
+    loadTasks();
+  
+    addTaskButton.addEventListener('click', () => {
+      if (taskInput.value.trim()) {
+        addTask(taskInput.value.trim());
+        taskInput.value = '';
+      }
+    });
+  
+    taskList.addEventListener('click', (event) => {
+      const listItem = event.target.parentElement.parentElement;
+      const taskId = listItem.dataset.taskId;
+  
+      if (event.target.classList.contains('delete-task')) {
+        removeTask(taskId);
+      } else if (event.target.classList.contains('mark-task')) {
+        toggleTaskDone(taskId);
+      }
+    });
+  
+    async function loadTasks() {
+      const response = await fetch('/tasks');
+      const tasks = await response.json();
+      tasks.forEach((task) => {
+        const listItem = createListItem(task);
+        taskList.appendChild(listItem);
+      });
+    }
+  
+    async function addTask(taskText) {
+      const task = { text: taskText, done: false };
+      const response = await fetch('/tasks', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(task)
+      });
+      if (response.status === 200) {
+        const newTask = await response.json();
+        const listItem = createListItem(newTask);
+        taskList.appendChild(listItem);
+      } else {
+        console.error('Error adding task:', response.statusText);
+      }
+      
+      const listItem = createListItem(newTask);
+      taskList.appendChild(listItem);
+    }
+  
+    function createListItem(task) {
+      const listItem = document.createElement('li');
+      listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+      listItem.dataset.taskId = task.id;
+      if (task.done) {
+        listItem.classList.add('done');
+        listItem.classList.add('list-group-item-success');
+      }
+      listItem.innerHTML = `
+        ${task.text}
+        <div>
+          <button class="btn btn-success btn-sm mark-task">${task.done ? 'Not Done' : 'Done'}</button>
+          <button class="btn btn-danger btn-sm delete-task">Delete</button>
+        </div>
+      `;
+      return listItem;
+    }
+  
+    async function removeTask(taskId) {
+      await fetch(`/tasks/${taskId}`, { method: 'DELETE' });
+      const listItem = taskList.querySelector(`[data-task-id="${taskId}"]`);
+      taskList.removeChild(listItem);
+    }
+  
+    async function toggleTaskDone(taskId) {
+      const listItem = taskList.querySelector(`[data-task-id="${taskId}"]`);
+      listItem.classList.toggle('done');
+      listItem.classList.toggle('list-group-item-success');
+      const markTaskButton = listItem.querySelector('.mark-task');
+      if (listItem.classList.contains('done')) {
+        markTaskButton.textContent = 'Not Done';
+      } else {
+        markTaskButton.textContent = 'Done';
+      }
+      await fetch(`/tasks/${taskId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ done: listItem.classList.contains('done') })
+      });
+    }
+  });
+  
